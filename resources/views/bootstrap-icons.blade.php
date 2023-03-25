c37844ced6d6a6003cb3a3c23357faa026b4570f
Azhar Lihan <m@azharlihan.com>
2023-03-25 20:33:42 +0700

    add test icons

--- resources/views/bootstrap-icons.blade.php
+++ resources/views/bootstrap-icons.blade.php
@@ -11,6 +11,11 @@
 <body>
     <h1>Test Bootstrap Icons From Node Modules</h1>
 
+    <i class="bi-alarm" style="font-size: 2rem; color: cornflowerblue;"></i>
+    <i class="bi-wifi" style="font-size: 2rem; color: gray;"></i>
+    <i class="bi bi-usb-plug" style="font-size: 10rem"></i>
+    <i class="bi bi-upload"></i>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </body>
 </html>
